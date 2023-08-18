37198d26a6f71e3214ea9984d0cbb1e8be678abc
jungwoo3490 <jungwoo3490@naver.com>
2023-08-18 13:15:12 +0900

    map 함수 사용

--- app/list/page.js
+++ app/list/page.js
@@ -1,13 +1,18 @@
 export default function List() {
+  let products = ['Tomatoes', 'Pasta', 'Coconut']
+
     return (
       <div >
         <h4 className='title'>상품목록</h4>
-        <div className="food">
-            <h4>상품1 $40</h4>
-        </div>
-        <div className="food">
-            <h4>상품2 $40</h4>
-        </div>
+        {
+          products.map((product) => {
+            return (
+              <div className="food">
+                <h4>{product} $40</h4>
+              </div>
+            )
+          })
+        }
       </div>
     )
   }
